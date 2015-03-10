305931a6e9a58dc013f6288c7308027201b3dea3
sttr110 <mydogs@live.jp>
2015-03-10 17:49:53 +0900

    さらに表示追加

--- WebSite/common/js/script.js
+++ WebSite/common/js/script.js
@@ -1,9 +1,27 @@
 $(function(){
+  //smart menu
   $("#toggle").click(function(){
     $("#menu").slideToggle();
     return false;
   });
   $("li").click(function(){
     $("#menu").hide();
+  })
+
+  //load more smart contents
+  $('#aboutMoreLoad').click(function(){
+    $('.hideAbout').slideToggle(1500);
+  })
+  $('#howtoMoreLoad').click(function(){
+    $('.hideHowto').slideToggle(1500);
+  })
+  $('#compareMoreLoad').click(function(){
+    $('.hideCompare').slideToggle(1500);
+  })
+  $('#iventMoreLoad').click(function(){
+    $('.hideIvent').slideToggle(1500);
+  })
+  $('#registerMoreLoad').click(function(){
+    $('.hideRegister').slideToggle(1500);
   })
 });
